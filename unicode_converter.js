bbcfa6598caf01bd9a1eb1f27e951aef20995e0e
Pablo <pablo.hehe2@gmail.com>
2023-09-09 17:44:08 -0300

    changes
    updated some identation and naming

--- unicode_converter.js
+++ unicode_converter.js
@@ -1,16 +1,16 @@
 String.prototype.style=function(code=6){
-tx = this.toLowerCase();
-abc="abcdefghijklmnopqrstuvwxyz";
-unicode=["ΛBÇĐΣFĠĦĮJҚŁMИΘPQЯŞŦU√ШXYZ",
+const tx = this.toLowerCase();
+const abc="abcdefghijklmnopqrstuvwxyz";
+const unicode=["ΛBÇĐΣFĠĦĮJҚŁMИΘPQЯŞŦU√ШXYZ",
 "ลвсอεғgнเјκℓмหσρφяธтนงฬҳчż",
 "ΛЪČĐΞҒĜĦΪĴЌĿMИΦPQЯŜŤŮ√ЩЖЧŻ",
 "αвс∂εғgнιјκłмησρqяѕтυνωxчz",
 "άвçđэƒģђıĵĸłмήǿρףяşтц√ώжγź",
 "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ",
 "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"];
-id=-1;
-  while(id++<abc.length){ 
+var id=-1;
+   while(id++<abc.length){
     tx=tx.replace(eval("/"+abc[id]+"/gi"), unicode[code][id]);
   }
-  return tx;
+return tx;
 }
